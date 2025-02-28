f67cb6998407fa478054971e3b0f3cec9b819c26
Sonava <sonava@code.edu.az>
2025-02-28 12:42:43 +0400

    Added Initial Admin Panel (May Change)

---
+++ EventEdu/Persentation/EventEdu.Webui/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventEdu.Webui.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

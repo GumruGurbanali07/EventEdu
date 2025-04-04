a9af1928e5424ade4fbf936bcf78c4ff0fabd62a
Sonava <sonava@code.edu.az>
2025-04-04 13:46:26 +0400

    Fixed UI Second

--- EventEdu/Persentation/EventEdu.Webui/Controllers/HomeController.cs
+++ EventEdu/Persentation/EventEdu.Webui/Controllers/HomeController.cs
@@ -12,13 +12,17 @@ public class HomeController : Controller
 	private readonly ILogger<HomeController> _logger;
 	private readonly IStringLocalizer<HomeController> _localizer;
 	private readonly IHeroSectionService _heroSectionService;
+    private readonly ICategoryService _categoryService;
+    private readonly IEventService _eventService;
 
-	public HomeController(ILogger<HomeController> logger, IHeroSectionService heroSectionService, IStringLocalizer<HomeController> localizer)
+    public HomeController(ILogger<HomeController> logger, IHeroSectionService heroSectionService, IStringLocalizer<HomeController> localizer, ICategoryService categoryService,
+        IEventService eventService)
 	{
 		_logger = logger;
 		_localizer = localizer;
 		_heroSectionService = heroSectionService;
-
+		_categoryService = categoryService;
+		_eventService = eventService;
 	}
 
 
@@ -27,11 +31,16 @@ public class HomeController : Controller
 	{
 		var lang = HttpContext.Session.GetString("lang") ?? "en-US";
 		var slider = await _heroSectionService.GetHeroSectionAll();
+		var category = await _categoryService.GetCategoriesAllAsync();
+		var events = await _eventService.GetEventAll();
 
 		var vm = new HomeIndexVM()
 		{
 			HeroSectionDetails = slider.Item2,
-			HeroSections = slider.Item1
+			HeroSections = slider.Item1,
+			CategoryDetails = category,
+			Events = events,
+			//Categories = category
 		};
 
 
